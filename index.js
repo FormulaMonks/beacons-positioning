ad4081103f9423ecee0e68d3bf5d5945b68dcd31
Luis Floreani <lucholaf@gmail.com>
2015-01-27 09:17:21 -0300

    saved logs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var io = require('socket.io')();
 var pkg = require(path.join(__dirname, 'package.json'));
 var beacons = require("./beacons");
+var fs = require("fs");
 var port = 3000;
 
 program.version(pkg.version).
@@ -25,10 +26,19 @@ io.on('connection', function(socket){
     });
 });
 
+// var logFilename = 'log.txt';
+// fs.writeFile(logFilename , '[');
+
+var startTime = new Date();
+
 beacons.waitForDevices = waitFor;
 beacons.on('update', function(devices) {
     charm.reset();
     devices.forEach(function(device) {
+        device["timestamp"] = (new Date() - startTime).toString();
+
+        // fs.appendFile(logFilename, JSON.stringify(device));
+        
         charm.write("distance: " + device.distance + "m device: " + device.name + " (rssi:" +  device.rssi + ")" + "\n");
     });
     io.emit('update', devices);
