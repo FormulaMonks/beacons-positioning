17428f2000a6b63074b5c4576e14f3ce26118798
Luis Floreani <luis.floreani@gmail.com>
2015-06-15 14:07:12 -0300

    refactoring chart config

--- HeatMapApp/BeaconsHeatmap/CBLogChartViewController.swift
+++ HeatMapApp/BeaconsHeatmap/CBLogChartViewController.swift
@@ -22,34 +22,50 @@ class CBLogChartViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        chartView.descriptionText = ""
+        let xAxis = chartView.xAxis;
+        xAxis.drawGridLinesEnabled = false
+        let yAxis = chartView.leftAxis;
+        yAxis.valueFormatter = NSNumberFormatter()
+        yAxis.valueFormatter?.maximumFractionDigits = 1
+        yAxis.valueFormatter?.positiveSuffix = " m"
+        let yAxisRight = chartView.rightAxis;
+        yAxisRight.valueFormatter = yAxis.valueFormatter
+
         loadChartData()
+    }
+    
+    func groupIntoAxisDataByMinor(var logs: NSArray) -> [Int : AxisData] {
+        var dataByMinor = [Int : AxisData]()
+        var i = 0
+        for val in logs {
+            let dict = val as! NSDictionary
+            let distance = dict["distance"] as! Double
+            let minor = dict["minor"] as! Int
+            let time = String(format:"%.1f", distance)
+            
+            var axisData = dataByMinor[minor];
+            if axisData == nil {
+                axisData = AxisData()
+                dataByMinor[minor] = axisData
+            }
+            
+            axisData?.xVals.append(time)
+            axisData?.yVals.append(ChartDataEntry(value: distance, xIndex: i++))
+        }
+        
+        return dataByMinor
     }
     
     func loadChartData() {
         let colors = [UIColor.redColor(), UIColor.greenColor(), UIColor.blueColor(), UIColor.blackColor(), UIColor.grayColor(), UIColor.cyanColor(), UIColor.brownColor(), UIColor.yellowColor()]
         
         if let values = logs {
-            var i = 0
-            var dataByMinor = [Int : AxisData]()
-            for val in values {
-                let dict = val as! NSDictionary
-                let distance = dict["distance"] as! Double
-                let minor = dict["minor"] as! Int
-                let time = String(format:"%f", distance)
-                
-                var axisData = dataByMinor[minor];
-                if axisData == nil {
-                    axisData = AxisData()
-                    dataByMinor[minor] = axisData
-                }
-                
-                axisData?.xVals.append(time)
-                axisData?.yVals.append(ChartDataEntry(value: distance, xIndex: i++))
-            }
+            var dataByMinor = groupIntoAxisDataByMinor(values)
             
             var dataSets = [LineChartDataSet]()
             var xVals = [String]()
-            i = 0
+            var i = 0
             for (key, value) in dataByMinor {
                 let dataSet = LineChartDataSet(yVals: value.yVals, label: String(format:"%d", key))
                 dataSets.append(dataSet)
