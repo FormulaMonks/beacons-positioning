fe1013cc9e670a5df04bdba45f957ca2b8f87538
Luis Floreani <luis.floreani@gmail.com>
2015-06-15 11:04:12 -0300

    adding chart functionality with swift

---
+++ HeatMapApp/BeaconsHeatmap/CBLogChartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CBLogChartViewController.swift
+//  BeaconsHeatmap
+//
+//  Created by Luis Floreani on 6/15/15.
+//  Copyright (c) 2015 Citrusbyte LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CBLogChartViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("loaded");
+    }
+}
